a0646a28ce3b2d67b12402382166e38cbf33c103
James Reed <jrreed84@gmail.com>
2015-09-13 00:26:48 -0700

    Prepping for implementation of detail master view by making TrackList and TrackPlayer activities create their Fragments dynamically.

--- app/src/main/java/com/plainjimbo/spotifystreamer/ArtistTrackListActivity.java
+++ app/src/main/java/com/plainjimbo/spotifystreamer/ArtistTrackListActivity.java
@@ -1,11 +1,9 @@
 package com.plainjimbo.spotifystreamer;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by jamesreed on 7/11/15.
- */
 public class ArtistTrackListActivity extends AppCompatActivity {
     public static final String EXTRA_ARTIST = "artist";
 
@@ -13,11 +11,19 @@ public class ArtistTrackListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_track_list);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.artist_track_list_container, new ArtistTrackListFragment())
+                    .commit();
+        }
         initSubtitle();
     }
 
     private void initSubtitle() {
         ArtistListItem artist = getIntent().getParcelableExtra(EXTRA_ARTIST);
-        getSupportActionBar().setSubtitle(artist.getName());
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null && artist != null) {
+            getSupportActionBar().setSubtitle(artist.getName());
+        }
     }
 }
--- app/src/main/java/com/plainjimbo/spotifystreamer/TrackPlayerActivity.java
+++ app/src/main/java/com/plainjimbo/spotifystreamer/TrackPlayerActivity.java
@@ -3,14 +3,19 @@ package com.plainjimbo.spotifystreamer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-
 public class TrackPlayerActivity extends AppCompatActivity {
     public static final String EXTRA_ARTIST = "artist";
     public static final String EXTRA_TRACK_INDEX = "trackIndex";
     public static final String EXTRA_TRACK_LIST = "trackList";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_player);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.track_player_container, new TrackPlayerFragment())
+                    .commit();
+        }
     }
 }
